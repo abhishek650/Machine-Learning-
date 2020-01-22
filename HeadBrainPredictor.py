88c05cc8ffca43c903d270afefb8d26c1def58f3
abhishek650 <60173494+abhishek650@users.noreply.github.com>
2020-01-22 08:42:00 -0800

    Machine Learning Casestudy

---
+++ HeadBrainPredictor.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+def HeadBrainPredictor():
+  
+  #Load Data
+  data = pd.read_csv('HeadBrain.csv')
+  
+  print("Size if data set",data.shape)
+  
+  X = data['Head Size(cm^3)'].values
+  Y = data['Braib Weight(grams)].values
+  
+  X = X.reshape((-1,1))
+  
+  n = len(X)
+  
+  reg = LinearRegression()
+  
+  reg = reg.fit(X,Y)
+  
+  y_pred = reg.predict(X)
+  
+  r2 = reg.score(X,Y)
+  
+  print(r2)
+
+def main():
+  print("---------Machine Learning Casestudy---------")
+  
+  print("----Survived Machine Learning-----")
+  
+  print("Linear Regression on Head and Brain size data set")
+  
+  HeadBrainPredictor()
+
+
+
+if __name__ == "__main__":
+  main()
